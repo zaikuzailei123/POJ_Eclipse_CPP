02361eaef5065f96b0e6ab0c59d7d87688aeb8ee
wontun12 <1270447834@qq.com>
2017-09-10 11:00:34 +0800

    test

---
+++ src/1.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : 1.cpp
+// Author      : 1
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main2() {
+	printf("你好！");
+}
